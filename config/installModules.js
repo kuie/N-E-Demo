9e87fa477f90c6b7f18e58f797d4f1b32d7fc2bc
zp_field <zp_field@163.com>
2018-11-16 17:00:59 +0800

    创建 package.json 控制部分 可根据需要调整package.json 依赖包 减少build 后可执行文件大小

---
+++ config/installModules.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+const
+    packagePath = path.resolve('./', 'package.json'),
+    basePackagePath = path.resolve('./', 'config', 'package_base.json'),
+    baseJson = require(basePackagePath),
+    nwJson = require(path.resolve('./', 'config', `package_nw.json`)),
+    electronJson = require(path.resolve('./', 'config', `package_electron.json`));
+let json = baseJson;
+json.dependencies = Object.assign(baseJson.dependencies, nwJson.dependencies, electronJson.dependencies);
+json.devDependencies = Object.assign(baseJson.devDependencies, nwJson.devDependencies, electronJson.devDependencies);
+fs.writeFileSync(packagePath, JSON.stringify(json, null, '  '), 'utf-8');
